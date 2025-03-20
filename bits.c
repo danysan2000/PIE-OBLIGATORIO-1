892514d582e62a0013129eb25db17bc679842350
Daniel Sanchez Latronico <alberto.daniel.sanchez@pcunix113.fing.edu.uy>
2025-03-20 17:23:21 -0300

    sumando funciones y archivos para la libreria libbits.a

---
+++ bits.c
@@ -0,0 +1,83 @@
+#include "bits.h"
+
+
+unsigned int bit(unsigned int buffer, int nb)
+{
+
+
+}    
+
+/*
+buffer: como entero sin signo.
+nb: como entero.
+nombreArchivo: como puntero a FILE.
+ */
+
+int ver_binario(unsigned int buffer, int nb, FILE* nombreArchivo)
+{
+
+
+}
+
+/*
+buffer: como entero sin signo.
+nb: como entero
+ */
+void print_binario(unsigned int buffer, int nb)
+{
+
+
+}
+
+/*
+buffer: como entero sin signo.
+nb: como entero.
+val: como entero
+*/
+unsigned int setbit(unsigned int buffer, int nb, int val)
+{
+
+}
+
+/*
+buffer: como entero sin signo.
+PIE Obligatorio 1 - Biblioteca de trabajo con bits 8
+codigo: como entero sin signo.
+nb: como entero.
+ */
+unsigned int concatena(unsigned int buffer, unsigned int codigo, int nb)
+{
+
+}
+
+
+/*
+max: como entero.
+min: como entero
+ */
+unsigned int crear_mascara(int max, int min)
+{
+
+}
+
+/*
+in: como entero sin signo.
+nbits: como entero
+ */
+unsigned int espejar(unsigned int in, int nbits)
+{
+
+}
+
+
+/*
+buffer: como entero sin signo.
+min: como entero.
+max: como entero
+ */
+unsigned int extraer(unsigned int buffer, int min, int max)
+{
+
+}
+
+
